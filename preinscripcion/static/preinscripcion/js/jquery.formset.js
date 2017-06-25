254fd42fb99408277f48aa84e65f5389adf4efe0
gabriel <gabriel.hruza@gmail.com>
2017-06-25 11:56:02 -0300

    cambios menores en el formset con js

--- preinscripcion/static/preinscripcion/js/jquery.formset.js
+++ preinscripcion/static/preinscripcion/js/jquery.formset.js
@@ -259,6 +259,22 @@
                     addButton.hide();
                 }
 
+                /////////////////////////////////////////////////////////////777
+                //copio los valores del formulario original en los que agrega
+                row_original_fn = $('#id_postulante-fecha_nacimiento').val();
+                row_original_ap = $('#id_postulante-apellido').val();
+                row_original_dm = $('#id_postulante-domicilio').val();
+
+                row_new_fn = '#id_'+options.prefix+'-'+formCount+'-fecha_nacimiento';
+                row_new_ap = '#id_'+options.prefix+'-'+formCount+'-apellido';
+                row_new_dm = '#id_'+options.prefix+'-'+formCount+'-domicilio';
+                
+                $(row_new_fn).val(row_original_fn);
+                $(row_new_ap).val(row_original_ap);
+                $(row_new_dm).val(row_original_dm);
+                //row
+                ///////////////////////////////////////////////////////////////7
+
                 // If a post-add callback was supplied, call it with the added form:
                 if (options.callbacks.onAdd) options.callbacks.onAdd(row, formCount);
                 return false;
